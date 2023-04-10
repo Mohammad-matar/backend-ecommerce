8203fd4063273014c9426b139a7b291bb6f6bcf5
Matarov <moe.matar1998@gmail.com>
2023-04-10 15:00:05 +0300

    adding DB in app.use with env file and port

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
+require("dotenv").config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var personalInfo = require('./routes/personalInfo');
+
 
 var app = express();
+const mongoose = require("mongoose");
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/category', personalInfo);
+
+
+mongoose
+    .connect(process.env.URL, {
+        dbName: process.env.DB_NAME,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connected Successfully to the Database");
+    })
+    .catch(console.error);
+
+//error handling
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).send({
+        success: false,
+        message: err.message,
+    });
+});
+
+// Undefined routes error handling
+app.use((req, res, next) => {
+    next(createError(404));
+});
 
 module.exports = app;
