d8695f4eb354dbb02b09a2cb809aa6ea8a6e9114
ahmad badawi <ahmad.badawu12@gmail.com>
2023-04-10 17:14:42 +0300

    creatr crud category

---
+++ Models/categoryModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const categorySchema = new mongoose.Schema({
+    
+    type: {
+        type: String,
+        required: [true, "Please enter the category"],
+        minLength: 3,
+        trim: true
+    },
+  description: {
+        type: String,
+        minLength: 3,
+        trim: true
+    },
+
+},
+    {
+        timestamps: true
+    }
+);
+
+
+module.exports = mongoose.model("Category", categorySchema);
--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
+var mongoose = require('mongoose')
+var createError =require("http-errors")
 
-// var personalInfo = require('./routes/personalInfo');
 
-
+var categories = require('./routes/categories');
 var app = express();
-const mongoose = require("mongoose");
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,8 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-
-// app.use('/category', personalInfo);
 
 
 mongoose
@@ -32,6 +32,7 @@ mongoose
     })
     .catch(console.error);
 
+    app.use("/api/category", categories);
 //error handling
 app.use((err, req, res, next) => {
     res.status(err.status || 500).send({
@@ -44,5 +45,6 @@ app.use((err, req, res, next) => {
 app.use((req, res, next) => {
     next(createError(404));
 });
+
 
 module.exports = app;
