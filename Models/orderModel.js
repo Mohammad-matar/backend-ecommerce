37a4aa682fec972897ac46f1a9dc48680b92dd39
Matarov <moe.matar1998@gmail.com>
2023-04-11 16:30:05 +0300

    order model schema

--- Models/orderModel.js
+++ Models/orderModel.js
@@ -1,20 +1,37 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const categorySchema = new mongoose.Schema({
-    
-    type: {
-        type: String,
-        required: [true, "Please enter the category"],
-        minLength: 3,
-        trim: true
-    },
-  description: {
-        type: String,
-        minLength: 3,
-        trim: true
-    },
+const orderSchema = new mongoose.Schema({
 
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    cart_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Cart',
+        required: true
+    },
+    shippingAddress: {
+        address: { type: String, required: true },
+        city: { type: String, required: true },
+        postalCode: { type: String, required: true },
+        country: { type: String, required: true }
+    },
+    paymentMethod: { type: String, required: true },
+
+    paymentResult: {
+        id: { type: String },
+        status: { type: String },
+        update_time: { type: String },
+        email_address: { type: String }
+    },
+    totalPrice: { type: Number, required: true },
+    isPaid: { type: Boolean, required: true },
+    paidAt: { type: Date },
+    isDelivered: { type: Boolean, required: true },
+    deliveredAt: { type: Date }
 },
     {
         timestamps: true
@@ -22,4 +39,4 @@ const categorySchema = new mongoose.Schema({
 );
 
 
-module.exports = mongoose.model("Category", categorySchema);
+module.exports = mongoose.model("Order", orderSchema);
